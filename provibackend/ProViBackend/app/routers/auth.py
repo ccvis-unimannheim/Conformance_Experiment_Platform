4ef09e03fd753aa329e9f0bb16b5b045c3171494
Tayyaba Zafar <tayyaba.zaf@gmail.com>
2026-05-20 23:02:21 +0200

    fix volume mount for SVG persistence

--- provibackend/ProViBackend/app/routers/auth.py
+++ provibackend/ProViBackend/app/routers/auth.py
@@ -41,7 +41,11 @@ async def auth(body: ds.PreliminaryAnswersRequest):
     dbc.create_document("User", user_doc)
 
     response = JSONResponse(content={"message": "User created."})
+<<<<<<< Updated upstream
     response.set_cookie(key="provi_user_id", value=user_id, expires=get_expiry(), secure=_COOKIE_SECURE, samesite=_COOKIE_SAMESITE)
+=======
+    response.set_cookie(key="provi_user_id", value=user_id, expires=get_expiry(), secure=False, samesite="lax")
+>>>>>>> Stashed changes
     return response
 
 
